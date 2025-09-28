ae508011bdfd2ffeb8bea20104bfbeb5775651a4
Bleron Hajdini <bleron.hajdini@stud.hslu.ch>
2025-09-28 14:36:12 +0200

    implementing diffrent methods to FormController

--- src/main/java/ch/hslu/devops/g01/backend/controller/FormController.java
+++ src/main/java/ch/hslu/devops/g01/backend/controller/FormController.java
@@ -1,12 +1,49 @@
 package ch.hslu.devops.g01.backend.controller;
 
+import ch.hslu.devops.g01.backend.entity.Form;
+import ch.hslu.devops.g01.backend.repository.FormRepository;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @Controller("/form")
 public class FormController {
 
-    @Get(uri="/", produces="text/plain")
-    public String index() {
-        return "Example Response";
+    private final FormRepository repository;
+
+    public FormController(FormRepository repository){
+        this.repository = repository;
     }
+
+    @Get("/")
+    public Iterable<Form> all(){
+        return repository.findAll();
+    }
+
+    @Post("/")
+    public HttpResponse<Form> create(@Body Form form){
+        var saved = repository.save(form);
+        return HttpResponse.created(saved);
+    }
+
+    @Delete("delete/{id}")
+    public HttpResponse<?> delete(UUID id){
+        if (repository.existsById(id)){
+            repository.deleteById(id);
+            return HttpResponse.noContent();
+        }
+        return HttpResponse.notFound();
+    }
+
+    @Get("/get/{id}")
+    public HttpResponse<Form> getById(UUID id){
+        return repository.findById(id)
+                .map(HttpResponse::ok)
+                .orElse(HttpResponse.notFound());
+    }
+
+
+
 }
