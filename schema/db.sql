00e1c68f6ddadb6739b4d573ee3669aab5c9b967
Bleron Hajdini <bleron.hajdini@stud.hslu.ch>
2025-10-03 15:41:17 +0200

    changin db schema

--- schema/db.sql
+++ schema/db.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS form_db;
 
 CREATE TABLE form_db (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     vorname TEXT NOT NULL,
     nachname TEXT NOT NULL,
     email TEXT NOT NULL
