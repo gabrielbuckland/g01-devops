00be0745023e43a7c76b538e44c9ecc1d206609c
Bleron Hajdini <bleron.hajdini@stud.hslu.ch>
2025-10-04 10:21:08 +0200

    refactor

--- backend/src/main/java/ch/hslu/devops/g01/FormController.java
+++ backend/src/main/java/ch/hslu/devops/g01/FormController.java
@@ -3,13 +3,13 @@ package ch.hslu.devops.g01;
 import ch.hslu.devops.g01.backend.entity.Form;
 import ch.hslu.devops.g01.backend.repository.FormRepository;
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.*;
+import io.micronaut.http.exceptions.HttpStatusException;
 import io.micronaut.validation.Validated;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 
-import java.util.List;
-import java.util.UUID;
 
 @Validated
 @Controller("/form")
@@ -28,6 +28,9 @@ public class FormController {
 
     @Post("/")
     public HttpResponse<Form> create(@Valid @Body Form form){
+        if(repository.existsById(form.getEmail())){
+            throw new HttpStatusException(HttpStatus.CONFLICT, "Email already exists!");
+        }
         var saved = repository.save(form);
         return HttpResponse.created(saved);
     }
