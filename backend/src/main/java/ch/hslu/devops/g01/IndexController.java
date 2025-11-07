afec1e729eda72f5e5e91d0234ce5e19dd25176a
Nevilan Sabanathan <nevilan.sabanathan@stud.hslu.ch>
2025-11-07 14:49:46 +0100

    return commit sha on the base endpoint

--- backend/src/main/java/ch/hslu/devops/g01/IndexController.java
+++ backend/src/main/java/ch/hslu/devops/g01/IndexController.java
@@ -1,12 +1,20 @@
 package ch.hslu.devops.g01;
 
+import io.micronaut.context.annotation.Value;
+import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.annotation.*;
 
 @Controller("/")
 public class IndexController {
 
-    @Get(uri="/", produces="text/plain")
-    public String index() {
-        return "Example Response";
+    @Value("${COMMIT_SHA:unknown}")
+    @Nullable
+    private String commitSha;
+
+    @Get()
+    public CommitDto commit() {
+        return new CommitDto(commitSha == null ? "unknown" : commitSha);
     }
+
+    public record CommitDto(String sha) { }
 }
